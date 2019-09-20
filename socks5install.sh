15dc0fbc9448ad3528b7e1392125dfa09956553d
pylist <suvsuv@vip.qq.com>
2019-09-20 14:52:39 +0800

    修改下载链接为github

--- socks5install.sh
+++ socks5install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-wget -O sock5.py http://pylist.org/sock5.py
+wget -O sock5.py https://raw.githubusercontent.com/pylist/proxy/master/sock5.py
 yum install epel-release
 yum install python36
 python3 $HOME/sock5
