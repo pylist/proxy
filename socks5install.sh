994ca1f4a897c175c82bba8a424cc352a2196956
jiuxian <52667561+pylist@users.noreply.github.com>
2021-09-04 13:58:12 +0800

    Update socks5install.sh
    更新python3版本

--- socks5install.sh
+++ socks5install.sh
@@ -3,5 +3,5 @@
 wget -O sock5.py https://raw.githubusercontent.com/pylist/proxy/master/sock5.py
 yum install epel-release -y
 yum makecache -y
-yum install python36 -y
+yum install python3 -y
 python3 $HOME/sock5.py
